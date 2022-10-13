41c968ebd5fc915c4781ed4f246b37d1c1cd3194
Nikola Hristov <nikola@nikolahristov.tech>
2022-10-13 22:24:13 +0300

    @playform/scripts@0.0.7

--- scripts/src/lib/utils.ts
+++ scripts/src/lib/utils.ts
@@ -1,6 +1,11 @@
 import fs from "fs";
 import { pathToFileURL } from "node:url";
+import { dirname, resolve } from "path";
 import ts from "typescript";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 /**
  * It takes a file path, checks if it's a TypeScript file, if it is, it compiles it to JavaScript, and
@@ -16,7 +21,9 @@ export const importFile = async (file: string) => {
 			JSON.parse(
 				(
 					await fs.promises.readFile(
-						"node_modules/@playform/config/tsconfig.json"
+						resolve(
+							`${__dirname}/../../node_modules/@playform/config/tsconfig.json`
+						)
 					)
 				).toString()
 			).compilerOptions,
