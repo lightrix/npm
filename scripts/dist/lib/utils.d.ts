b5b24975363491889e43bdd0ff4159e7d40861e0
Nikola Hristov <nikola@nikolahristov.tech>
2022-10-15 05:00:45 +0300

    0.0.8

--- scripts/dist/lib/utils.d.ts
+++ scripts/dist/lib/utils.d.ts
@@ -1,7 +1,7 @@
 /**
  * It takes a file path, checks if it's a TypeScript file, if it is, it compiles it to JavaScript, and
  * then imports it
- * @param {string} file - The path to the file to import.
+ * @param {string} file - The file to import.
  * @returns The default export of the file.
  */
 export declare const importFile: (file: string) => Promise<any>;
