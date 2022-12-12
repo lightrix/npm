de2b2042f93b4d5c2f8f8e72f7545ca588671e0c
Nikola Hristov <nikola@nikolahristov.tech>
2022-12-12 12:17:52 +0200

    squash!

--- scripts/src/lib/getJson.ts
+++ scripts/src/lib/getJson.ts
@@ -2,11 +2,12 @@ import * as fs from "fs";
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 
-const __filename = fileURLToPath(import.meta.url);
-
 export default async (glob: string) =>
 	JSON.parse(
 		(
-			await fs.promises.readFile(`${dirname(__filename)}${glob}`, "utf-8")
+			await fs.promises.readFile(
+				`${dirname(fileURLToPath(import.meta.url))}${glob}`,
+				"utf-8"
+			)
 		).toString()
 	);
